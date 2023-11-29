b1f9f25ad8cb6f405c1d856928140515f9003acf
Gleice Kelly <gleice.kelly22@hotmail.com>
2023-11-29 19:44:56 -0300

    Aplicação da funcionalidade

---
+++ script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const sons = {
+    'A': 'boom.wav',
+    'S': 'clap.wav',
+    'D': 'hihat.wav',
+    'F': 'kick.wav',
+    'G': 'openhat.wav',
+    'H': 'ride.wav',
+    'K': 'tink.wav',
+    'L': 'tom.wav'
+}
+
+function criarDiv(texto) {
+    const div = document.createElement('div');
+    div.classList.add('key');
+    div.textContent = texto;
+    div.id = texto;
+    document.getElementById('container').appendChild(div);
+}
+
+const exibir = (sons) => Object.keys(sons).forEach(criarDiv);
+
+const tocarSom = (letra) => {
+    const audio = new Audio(`./sounds/${sons[letra]}`); /* endereço do som, pega {sons, e a [posição letra escolhida]} */
+    audio.play();
+}
+
+const adicionarEfeito = (letra) => document.getElementById(letra).classList.add('active');
+
+const removerEfeito = (letra) => document.getElementById(letra).classList.remove('active');
+
+const ativarId = (Event) => {
+    const letra = Event.target.id;
+    const letraPermitida = sons.hasOwnProperty(letra);
+    if (letraPermitida) {
+        adicionarEfeito(letra);
+        tocarSom(letra);
+        removerEfeito(letra);
+    }
+}
+
+exibir(sons);
+
+document.getElementById('container').addEventListener('click', ativarId);
+
+window.addEventListener('keyup', ativarId);
